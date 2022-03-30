382c0558a6300f5cb9f54df8b2d77450d05e58ec
Andrés Camilo Yáñez Escobar <andrescamiloye@ufps.edu.co>
2022-03-30 18:37:57 -0500

    Calculando los posibles movimientos de la pieza del caballo.

--- app/Models/Chess/ChessPiece.php
+++ app/Models/Chess/ChessPiece.php
@@ -52,4 +52,34 @@ class ChessPiece extends Model
     {
         return $this->hasMany(PieceMovement::class);
     }
+
+    /**
+     * Get all Piece Movements which are relationed with the ChessPiece in a specific position.
+     * 
+     * @param int $position_x
+     * @param int $position_y
+     * @return PieceMovement[] $pieceMovements
+     */
+    public function avaliableMovements($position_x, $position_y, $dimensions)
+    {
+        if ($position_x > 0 && $position_y > 0) {
+            return $this->movements->map(function ($movement) use ($position_x, $position_y, $dimensions) {
+                $min_X = $position_x + $movement->movement_x;
+                $min_Y = $position_y + $movement->movement_y;
+
+                $max_X =   $movement->movement_x + $position_x;
+                $max_Y = $movement->movement_y +  $position_y;
+
+                if (($max_X < $dimensions && $min_X > 0) && ($max_Y < $dimensions && $min_Y > 0)) {
+                    return $movement;
+                } else {
+                    return false;
+                }
+            })->reject(function ($value) {
+                return $value === false;
+            });
+        } else {
+            return [];
+        }
+    }
 }
